fa10e11406520669892efdd9cc72c61bdd48a005
Ronald C Edwards <rce246@stern.nyu.edu>
2020-01-16 05:06:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function logWhisper(string) {
 }
 
 
-function sayHitoGrandma(string) {if (string === "I love you, Grandma.")
+function sayHitoGrandma(`string`) {if (`string` === "I love you, Grandma.")
 {return "I love you, too."}
 }
